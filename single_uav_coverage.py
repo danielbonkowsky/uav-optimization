c3b3ea65136948e10b6a11c23753d748ef9fdfb8
danielbonkowsky <danielbonkowsky@gmail.com>
2025-05-06 13:51:46 -0700

    Initialized file for single uav coverage simulation

---
+++ single_uav_coverage.py
@@ -0,0 +1,5 @@
+"""
+Goal is to generate a plot showing the relationship between the angle of a UAV
+along its circular path and the achievable rate for a user in the dead zone 
+(average/integral of all possible points in the dead zone?)
+"""
