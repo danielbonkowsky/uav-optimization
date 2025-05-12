ed1cc0f5a9385497fb74827dc14e138201a394da
danielbonkowsky <danielbonkowsky@gmail.com>
2025-05-11 18:54:04 -0700

    Simplified model

--- single_uav_coverage.py
+++ single_uav_coverage.py
@@ -14,31 +14,73 @@ d : distance from BS to center of UAV flight circle
 Output
 rate : achievable rate in bits/s/hz
 """
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 UAV_HEIGHT = 30   # UAV circles 30m above the ground
 UAV_RADIUS = 30   # UAV circles with a radius of 30m
-USER_RADIUS = 30  # Dead zone is a circle with radius 30
+USER_RADIUS = 30  # Dead zone is a circle with radius 30m
 BS_DISTANCE = 100 # BS is 100m from the center of the UAV circle
 
+BS_TRANSMIT_POWER = 100
+UAV_TRANSMIT_POWER = 100
+NOISE_POWER = 0.01
 
-
-def uav_distance(theta):
-    """returns the distance from the BS to the UAV
-
-    Arguments:
-    theta -- the UAV's position along its flight path
-    """
-
-    return 0;
-
-def achievable_downlink_fixed(theta, x, y):
-    """returns the maximum achievable downlink (BS to user) rate in bits/s/hz 
-    assuming a fixed timeshare rate 
+def uav_distance(theta, x, y):
+    """returns the distance from a point to the UAV
 
     Arguments:
     theta -- the UAV's position along its flight path
-    x -- the user's x coordinate
-    y -- the user's y coordinate
+    x -- x-coord of the point
+    y -- y-coord of the point
+    """
+    
+    uav_x = UAV_RADIUS * math.cos(theta)
+    uav_y = UAV_RADIUS * math.sin(theta)
+
+    plane_distance = math.sqrt((uav_x - x)**2 + (uav_y - y)**2)
+    
+    return math.sqrt(UAV_HEIGHT**2 + plane_distance**2)
+
+def uav_downlink_rate(theta):
+    """returns the downlink rate achievable by the UAV
+
+    Arguments
+    theta -- the UAV's position along its flight path
     """
 
-    return 0;
+    d = uav_distance(theta, -BS_DISTANCE, 0)
+    power = BS_TRANSMIT_POWER / (d**2)
+    
+    return math.log2(1 + power/NOISE_POWER) 
+
+def user_downlink_rate(theta, x, y):
+    """returns the downlink rate achievable by the user
+
+    Arguments
+    theta -- the UAV's position along its flight path
+    x -- user x-coord
+    y -- user y-coord
+    """
+
+    d = uav_distance(theta, x, y)
+    power = UAV_TRANSMIT_POWER / (d**2)
+
+    return math.log2(1 + power/NOISE_POWER)
+
+theta = np.linspace(0, 2*math.pi, 100)
+uav_downlink = np.array([uav_downlink_rate(t) for t in theta])
+user_downlink = np.array([user_downlink_rate(t, UAV_RADIUS, 0) for t in theta])
+
+fig, ax = plt.subplots()
+
+ax.plot(theta, uav_downlink, color='red', label='uav downlink')
+ax.plot(theta, user_downlink, color='blue', label='user downlink')
+
+ax.set_xticks(np.arange(0, 9*math.pi/4, math.pi/4))
+ax.set_xlabel('theta (rad)')
+ax.set_ylabel('achievable rate')
+ax.legend(loc='upper left')
+
+plt.show()
