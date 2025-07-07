4b2d810e35664195478c234247a6aaea31268742
danielbonkowsky <danielbonkowsky@gmail.com>
2025-07-06 21:12:56 -0700

    Got a prototype of radius optimization working

--- radius_optimization.py
+++ radius_optimization.py
@@ -18,10 +18,12 @@ import math
 import scipy
 import random
 
-RADIUS_UAV = 30       # Radius of UAV flight path
-H = 100               # Height at which UAV circles
-A = 2000              # Distance from BS to center of UAV flight path
-B = 2500              # Distance from BS to center of dead zone
+RADIUS_MIN_UAV = 30 # Minimum radius that the UAV can circle
+RADIUS_MAX_UAV = 50 # Maximum radius that the UAV can circle
+STEP_SIZE = 0.1     # Step size that the program will go through radii
+H = 100             # Height at which UAV circles
+A = 2000            # Distance from BS to center of UAV flight path
+B = 2500            # Distance from BS to center of dead zone
 
 SIGNAL_FREQUENCY = 2*10**9
 SIGNAL_WAVELENGTH = scipy.constants.c / SIGNAL_FREQUENCY
@@ -41,7 +43,9 @@ USER_DISTRIBUTION = 'uniform' # Distribution of users in the dead zone
 
 ANTENNA_GAIN = 1 # GT, GR in power equations
 
-def gu_uav_distance(theta, x, z):
+VELOCITY_UAV = 21 # see energy-efficiency paper
+
+def gu_uav_distance(theta, radius, x, z):
     """returns the distance between a ground user and the UAV
 
     Arguments:
@@ -50,22 +54,22 @@ def gu_uav_distance(theta, x, z):
     z -- the z coordinate of the ground user
     """
 
-    return math.sqrt( (A + RADIUS_UAV * math.cos(theta) - x)**2
+    return math.sqrt( (A + radius * math.cos(theta) - x)**2
                     + H**2
-                    + (RADIUS_UAV * math.sin(theta) - z)**2 )
+                    + (radius * math.sin(theta) - z)**2 )
 
-def uav_bs_distance(theta):
+def uav_bs_distance(theta, radius):
     """returns distance between the UAV and base station
 
     Arguments:
     theta -- the UAV's position along its flight path
     """
 
-    return math.sqrt( (A + RADIUS_UAV * math.cos(theta)**2
+    return math.sqrt( (A + radius * math.cos(theta)**2
                     + H**2
-                    + (RADIUS_UAV * math.sin(theta))**2) )
+                    + (radius * math.sin(theta))**2) )
 
-def uav_receive_power(theta, x, z):
+def uav_receive_power(theta, radius, x, z):
     """returns the received power at the UAV from the user
 
     Arguments:
@@ -75,9 +79,9 @@ def uav_receive_power(theta, x, z):
     """
 
     return ( (POWER_GROUND_USER * ANTENNA_GAIN**2 * SIGNAL_WAVELENGTH**2)
-           / (4 * math.pi * gu_uav_distance(theta, x, z))**2 )
+           / (4 * math.pi * gu_uav_distance(theta, radius, x, z))**2 )
 
-def bs_receive_power(theta):
+def bs_receive_power(theta, radius):
     """returns the received power at the BS from the UAV
     
     Arguments:
@@ -85,7 +89,7 @@ def bs_receive_power(theta):
     """
 
     return ( (POWER_UAV * ANTENNA_GAIN**2 * SIGNAL_WAVELENGTH**2)
-           / (4 * math.pi * uav_bs_distance(theta))**2 )
+           / (4 * math.pi * uav_bs_distance(theta, radius))**2 )
 
 def generate_users():
     """returns a list of user coordinates generated based on how the users are 
@@ -98,6 +102,57 @@ def generate_users():
         for i in range(NUM_USERS):
             x = B + random.uniform(-RADIUS_DEADZONE, RADIUS_DEADZONE)
             z = random.uniform(-RADIUS_DEADZONE, RADIUS_DEADZONE)
-            user_coordinates.append((x, 0, z))
+            user_coordinates.append((x, z))
 
         return user_coordinates
+
+if (__name__ == '__main__'):
+
+    uav_thetas = [(2 * user + 1) * math.pi / NUM_USERS for user in range(NUM_USERS)]
+    user_positions = generate_users()
+
+    optimal_radius = RADIUS_MIN_UAV
+    optimal_bits_transmitted = 0
+
+    for i in range(int( (RADIUS_MAX_UAV - RADIUS_MIN_UAV) / STEP_SIZE )):
+
+        radius = RADIUS_MIN_UAV + i * STEP_SIZE
+
+        print(f'radius: {radius}')
+
+        period = (2 * math.pi * radius) / VELOCITY_UAV
+        timeslot_length = period / NUM_USERS
+
+        print(f'period: {period}')
+        print(f'timeslot_length: {timeslot_length}')
+
+        bits_transmitted = 0
+        for j in range(NUM_USERS):
+
+            uav_receive_power_W = 10 ** (uav_receive_power(uav_thetas[j], radius, user_positions[j][0], user_positions[j][1]) / 10)
+            noise_power_W = 10 ** (POWER_NOISE / 10)
+
+            uav_received_bits = ( 0.5 
+                                 * timeslot_length 
+                                 * BANDWIDTH 
+                                 * math.log2(1 
+                                             + uav_receive_power_W / noise_power_W ) )
+            
+            bs_receive_power_W = 10 ** (bs_receive_power(uav_thetas[j], radius) / 10)
+
+            bs_received_bits = ( 0.5 
+                                * timeslot_length 
+                                * BANDWIDTH 
+                                * math.log2(1 
+                                            + bs_receive_power_W / noise_power_W ) )
+
+            bits_transmitted += min(uav_received_bits, bs_received_bits)
+        
+        if (bits_transmitted > optimal_bits_transmitted):
+            optimal_bits_transmitted = bits_transmitted
+            optimal_radius = radius
+        
+        print()
+    
+    print(f'Optimal radius {optimal_radius} m')
+    print(f'{optimal_bits_transmitted} bits transmitted')
