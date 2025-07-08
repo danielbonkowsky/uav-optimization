6140ccf907753214ecb65aeb88ac6dcc8109dcb1
danielbonkowsky <danielbonkowsky@gmail.com>
2025-07-08 11:19:50 -0700

    Converted from dbW to W and added units to comments

--- radius_optimization.py
+++ radius_optimization.py
@@ -18,38 +18,40 @@ import math
 import scipy
 import random
 
-RADIUS_MIN_UAV = 30 # Minimum radius that the UAV can circle
-RADIUS_MAX_UAV = 50 # Maximum radius that the UAV can circle
-STEP_SIZE = 0.1     # Step size that the program will go through radii
-H = 100             # Height at which UAV circles
-A = 2000            # Distance from BS to center of UAV flight path
-B = 2500            # Distance from BS to center of dead zone
+RADIUS_MIN_UAV = 30   # Minimum radius that the UAV can circle in meters
+RADIUS_MAX_UAV = 1000 # Maximum radius that the UAV can circle in meters
+STEP_SIZE = 0.1       # Step size that the program will go through radii
+H = 100               # Height at which UAV circles in meters
+A = 2000              # Distance from BS to center of UAV flight path in meters
+B = 2500              # Distance from BS to center of dead zone in meters
 
 SIGNAL_FREQUENCY = 2*10**9
 SIGNAL_WAVELENGTH = scipy.constants.c / SIGNAL_FREQUENCY
 
-BANDWIDTH = 1*10**6 # UAV/BS/GU bandwidth (from energy-efficiency paper)
+BANDWIDTH = 1*10**6 # 1 MHz UAV/BS/GU bandwidth (from energy-efficiency paper)
 
-# Powers in dBW (see https://en.wikipedia.org/wiki/Decibel_watt)
+
 # power in dBW = 10 * log10(power / 1W)
 # power in W = 10^(power in dBW / 10)
-POWER_NOISE = 7.5 - 174 + 10 * math.log10(BANDWIDTH) # Noise power in dBW
-POWER_GROUND_USER = 10                               # User power in dBW
-POWER_UAV = 10                                       # UAV power in dBW (from energy-efficiency paper)
+POWER_NOISE = 10 ** ( 
+    (7.5 - 174 + 10 * math.log10(BANDWIDTH)) / 10 ) # Noise power in watts
+POWER_GROUND_USER = 1                               # User power in watts
+POWER_UAV = 1                                       # UAV power in watts
 
-RADIUS_DEADZONE = 100         # Radius of dead zone
+RADIUS_DEADZONE = 100         # Radius of dead zone in meters
 NUM_USERS = 50                # Number of users in the dead zone
 USER_DISTRIBUTION = 'uniform' # Distribution of users in the dead zone
 
 ANTENNA_GAIN = 1 # GT, GR in power equations
 
-VELOCITY_UAV = 21 # see energy-efficiency paper
+VELOCITY_UAV = 21 # m/s, see energy-efficiency paper
 
 def gu_uav_distance(theta, radius, x, z):
     """returns the distance between a ground user and the UAV
 
     Arguments:
     theta -- the UAV's position along its flight path
+    radius -- the radius of the UAV's flight path in meters
     x -- the x coordinate of the ground user
     z -- the z coordinate of the ground user
     """
@@ -63,6 +65,7 @@ def uav_bs_distance(theta, radius):
 
     Arguments:
     theta -- the UAV's position along its flight path
+    radius -- the radius of the UAV's flight path in meters
     """
 
     return math.sqrt( (A + radius * math.cos(theta)**2
@@ -70,22 +73,24 @@ def uav_bs_distance(theta, radius):
                     + (radius * math.sin(theta))**2) )
 
 def uav_receive_power(theta, radius, x, z):
-    """returns the received power at the UAV from the user
+    """returns the received power at the UAV from the user in watts
 
     Arguments:
-    theta -- the UAV's position along its flight path
-    x -- the x coordinate of the ground user
-    z -- the z coordinate of the ground user
+    theta -- the UAV's position along its flight path in rad
+    radius -- the radius of the UAV's flight path in meters
+    x -- the x coordinate of the ground user in meters
+    z -- the z coordinate of the ground user in meters
     """
 
     return ( (POWER_GROUND_USER * ANTENNA_GAIN**2 * SIGNAL_WAVELENGTH**2)
            / (4 * math.pi * gu_uav_distance(theta, radius, x, z))**2 )
 
 def bs_receive_power(theta, radius):
-    """returns the received power at the BS from the UAV
+    """returns the received power at the BS from the UAV in watts
     
     Arguments:
-    theta -- the UAV's position along its flight path
+    theta -- the UAV's position along its flight path in rad
+    radius -- the radius of the UAV's flight path in meters
     """
 
     return ( (POWER_UAV * ANTENNA_GAIN**2 * SIGNAL_WAVELENGTH**2)
@@ -106,53 +111,22 @@ def generate_users():
 
         return user_coordinates
 
+def get_uav_theta(radius, time):
+    """returns the UAV's position along its flight path given a time
+    
+    Arguments:
+        radius -- the radius of the UAV's flight path in meters
+        time -- the elapsed time since the beginning of simulation in seconds
+    """
+    period = (2 * math.pi * radius) / VELOCITY_UAV
+
+    return (2 * math.pi / period) * time
+
 if (__name__ == '__main__'):
 
-    uav_thetas = [(2 * user + 1) * math.pi / NUM_USERS for user in range(NUM_USERS)]
-    user_positions = generate_users()
+    experiment_time = (2 * math.pi * RADIUS_MAX_UAV) / VELOCITY_UAV
 
-    optimal_radius = RADIUS_MIN_UAV
-    optimal_bits_transmitted = 0
+    radius = RADIUS_MIN_UAV
 
-    for i in range(int( (RADIUS_MAX_UAV - RADIUS_MIN_UAV) / STEP_SIZE )):
-
-        radius = RADIUS_MIN_UAV + i * STEP_SIZE
-
-        print(f'radius: {radius}')
-
-        period = (2 * math.pi * radius) / VELOCITY_UAV
-        timeslot_length = period / NUM_USERS
-
-        print(f'period: {period}')
-        print(f'timeslot_length: {timeslot_length}')
-
-        bits_transmitted = 0
-        for j in range(NUM_USERS):
-
-            uav_receive_power_W = 10 ** (uav_receive_power(uav_thetas[j], radius, user_positions[j][0], user_positions[j][1]) / 10)
-            noise_power_W = 10 ** (POWER_NOISE / 10)
-
-            uav_received_bits = ( 0.5 
-                                 * timeslot_length 
-                                 * BANDWIDTH 
-                                 * math.log2(1 
-                                             + uav_receive_power_W / noise_power_W ) )
-            
-            bs_receive_power_W = 10 ** (bs_receive_power(uav_thetas[j], radius) / 10)
-
-            bs_received_bits = ( 0.5 
-                                * timeslot_length 
-                                * BANDWIDTH 
-                                * math.log2(1 
-                                            + bs_receive_power_W / noise_power_W ) )
-
-            bits_transmitted += min(uav_received_bits, bs_received_bits)
-        
-        if (bits_transmitted > optimal_bits_transmitted):
-            optimal_bits_transmitted = bits_transmitted
-            optimal_radius = radius
-        
-        print()
-    
-    print(f'Optimal radius {optimal_radius} m')
-    print(f'{optimal_bits_transmitted} bits transmitted')
+    while (radius < RADIUS_MAX_UAV):
+        pass
