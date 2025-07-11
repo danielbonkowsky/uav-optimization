d0292b30aeb7694e19478b5b7c1e392f7de938a8
danielbonkowsky <danielbonkowsky@gmail.com>
2025-07-11 10:22:10 -0700

    Modified radius optimization to run 1000 trials

--- radius_optimization.py
+++ radius_optimization.py
@@ -16,10 +16,13 @@
 
 import math
 import scipy
+import time
 import random
+import matplotlib.pyplot as plt
+import pandas as pd
 
-RADIUS_MIN_UAV = 30   # Minimum radius that the UAV can circle in meters
-RADIUS_MAX_UAV = 1000 # Maximum radius that the UAV can circle in meters
+RADIUS_MIN_UAV = 30  # Minimum radius that the UAV can circle in meters
+RADIUS_MAX_UAV = 60   # Maximum radius that the UAV can circle in meters
 STEP_SIZE = 0.1       # Step size that the program will go through radii
 H = 100               # Height at which UAV circles in meters
 A = 2000              # Distance from BS to center of UAV flight path in meters
@@ -39,7 +42,7 @@ POWER_GROUND_USER = 1                               # User power in watts
 POWER_UAV = 1                                       # UAV power in watts
 
 RADIUS_DEADZONE = 100         # Radius of dead zone in meters
-NUM_USERS = 50                # Number of users in the dead zone
+NUM_USERS = 1                # Number of users in the dead zone
 USER_DISTRIBUTION = 'uniform' # Distribution of users in the dead zone
 
 ANTENNA_GAIN = 1 # GT, GR in power equations
@@ -72,26 +75,28 @@ def uav_bs_distance(theta, radius):
                     + H**2
                     + (radius * math.sin(theta))**2) )
 
-def uav_receive_power(theta, radius, x, z):
+def uav_receive_power(radius, time, x, z):
     """returns the received power at the UAV from the user in watts
 
     Arguments:
-    theta -- the UAV's position along its flight path in rad
     radius -- the radius of the UAV's flight path in meters
+    time -- the elapsed time since the beginning of simulation in seconds
     x -- the x coordinate of the ground user in meters
     z -- the z coordinate of the ground user in meters
     """
+    theta = get_uav_theta(radius, time)
 
     return ( (POWER_GROUND_USER * ANTENNA_GAIN**2 * SIGNAL_WAVELENGTH**2)
            / (4 * math.pi * gu_uav_distance(theta, radius, x, z))**2 )
 
-def bs_receive_power(theta, radius):
+def bs_receive_power(radius, time):
     """returns the received power at the BS from the UAV in watts
     
     Arguments:
-    theta -- the UAV's position along its flight path in rad
     radius -- the radius of the UAV's flight path in meters
+    time -- the elapsed time since the beginning of simulation in seconds
     """
+    theta = get_uav_theta(radius, time)
 
     return ( (POWER_UAV * ANTENNA_GAIN**2 * SIGNAL_WAVELENGTH**2)
            / (4 * math.pi * uav_bs_distance(theta, radius))**2 )
@@ -123,10 +128,64 @@ def get_uav_theta(radius, time):
     return (2 * math.pi / period) * time
 
 if (__name__ == '__main__'):
+    start_time = time.time()
 
-    experiment_time = (2 * math.pi * RADIUS_MAX_UAV) / VELOCITY_UAV
+    longest_period = (2 * math.pi * RADIUS_MAX_UAV) / VELOCITY_UAV
+    
+    data = pd.DataFrame()
+    optimal_radii = []
 
-    radius = RADIUS_MIN_UAV
+    for it in range(1000):
 
-    while (radius < RADIUS_MAX_UAV):
-        pass
+        user = generate_users()[0]
+        radius = RADIUS_MIN_UAV
+
+        max_bits = -1
+        optimal_radius = -1
+
+        radii = []
+        bits = []
+
+        while (radius <= RADIUS_MAX_UAV):
+            uav_received_bits = 0
+            bs_received_bits = 0
+
+            for i in range(int(longest_period * 1000)):
+
+                if (i % 2 == 0):
+                    bps = BANDWIDTH * math.log2(1 + uav_receive_power(radius,
+                                                                    i / 1000.0,
+                                                                    user[0],
+                                                                    user[1]) 
+                                                    / POWER_NOISE)
+                    uav_received_bits += bps / 1000
+                else:
+                    bps = BANDWIDTH * math.log2(1 + bs_receive_power(radius,
+                                                                    i / 1000.0)
+                                                    / POWER_NOISE)
+                    bs_received_bits += bps / 1000
+            
+            if (min(bs_received_bits, uav_received_bits) > max_bits):
+                max_bits = min(bs_received_bits, uav_received_bits)
+                optimal_radius = radius
+
+            radii.append(radius)
+            bits.append(min(uav_received_bits, bs_received_bits))
+            
+            radius += STEP_SIZE
+
+        optimal_radii.append(optimal_radius)
+        data[f'radii{it}'] = radii
+        data[f'bits{it}'] = bits
+    
+    for i in range(1000):
+        plt.plot(data[f'radii{i}'], data[f'bits{i}'])
+        plt.axvline(x = optimal_radii[i])
+    
+    plt.xlabel('Radius (m)')
+    plt.ylabel('Bits transmitted')
+
+    print(f'Runtime: {time.time() - start_time} seconds')
+
+    plt.show()
+
