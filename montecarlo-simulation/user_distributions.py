49b4f4d3fb4ee86192db6f0e5e0123110072cca0
danielbonkowsky <danielbonkowsky@gmail.com>
2025-09-10 10:11:45 -0600

    Updated user distributions

--- montecarlo-simulation/user_distributions.py
+++ montecarlo-simulation/user_distributions.py
@@ -31,23 +31,64 @@ def oneFarOneClose(num_users, mean, std, seed=324):
             tmp_users += [ (x2, y2) ]
     users += tmp_users
     
-    return users
+    return np.asarray(users)
 
-def skewedNormal(num_users, mean, std, seed=324):
+def leftSkewedNormal(num_users, mean, std, seed=324):
+    random.seed(seed)
+    np.random.seed(seed)
+    rng = np.random.default_rng(seed)
+
+    users = []
+    while (len(users) < num_users):
+        x = rng.normal(loc=mean+3000, scale=std)
+        y = rng.normal(loc=0, scale=std)
+
+        if ((x < mean and abs(y) < std) or (x - mean)**2 + y**2 < std**2):
+            if (x**2 + y**2 > 4000000):
+                users.append((x, y))
+    
+    return np.asarray(users)
+
+def uniform(num_users, center, radius, seed=324):
     random.seed(seed)
     np.random.seed(seed)
     rng = np.random.default_rng(seed)
 
     users = []
 
-    x_mean = mean + 3000
-    y_mean = 0
-    while (len(users) < num_users):
-        x = rng.normal(loc=x_mean, scale=std)
-        y = rng.normal(loc=y_mean, scale=std)
+    for _ in range(num_users):
+            r = radius * np.sqrt(random.random())
+            theta = random.uniform(0, 2*np.pi)
+            x = center + r * np.cos(theta)
+            y = r * np.sin(theta)
+    
+            users.append((x, y))
+    
+    return np.asarray(users)
 
-        if ((x < mean and abs(y) < std) or (x - mean)**2 + y**2 < std**2):
+def leftUpSkewedNormal(num_users, mean1, std1, mean2, std2, seed=324):
+    random.seed(seed)
+    np.random.seed(seed)
+    rng = np.random.default_rng(seed)
+
+    users = []
+
+    # left skewed
+    while (len(users) < 0.5*num_users):
+        x = rng.normal(loc=mean1, scale=std1)
+        y = rng.normal(loc=0, scale=std1)
+
+        if ((x < mean1 and abs(y) < std1) or (x - mean1)**2 + y**2 < std1**2):
             if (x**2 + y**2 > 4000000):
                 users.append((x, y))
     
-    return users
+    # up skewed
+    while (len(users) < num_users):
+        x = rng.normal(loc=mean2, scale=std2)
+        y = rng.normal(loc=0, scale=std2)
+
+        if ((y > 0 and abs(x - mean2) < std2) or (x - mean2)**2 + y**2 < std2**2):
+            if (x**2 + y**2 > 4000000):
+                users.append((x, y))
+    
+    return np.asarray(users)
